d2617e5801d217dc4ac5222f33ec0a5144718800
Rishabh Gupta <programmer.rishabh@gmail.com>
2024-08-22 10:56:57 +0530

    Character Demo

---
+++ CharDemo/src/CharDemo.java
@@ -0,0 +1,9 @@
+class CharDemo {
+    public static void main(String[] args) {
+        char ch1, ch2;
+        ch1 = 88;
+        ch2 = 'Y';
+
+        System.out.println("ch1 and ch2: " + ch1 + " " + ch2);
+    }
+}
---
+++ CharDemo2/src/CharDemo2.java
@@ -0,0 +1,11 @@
+class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+        ch1 = 'X';
+        System.out.println("Char ch1 contains " + ch1);
+
+        ch1++;
+
+        System.out.println("Ch1 is now " + ch1);
+    }
+}
