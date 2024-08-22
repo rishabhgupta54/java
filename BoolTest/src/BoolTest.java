345ac2aba4c6b70f32227380ef79495611c8959c
Rishabh Gupta <programmer.rishabh@gmail.com>
2024-08-22 12:46:15 +0530

    Added the example for boolean test, Dynamic Initialision of vairable and scope of variable

---
+++ BoolTest/src/BoolTest.java
@@ -0,0 +1,17 @@
+class BoolTest {
+    public static void main(String[] args) {
+        boolean b;
+        b = false;
+        System.out.println("b is " + b);
+
+        b = true;
+        System.out.println("b is " + b);
+
+        if (b) {
+            System.out.println("This is executed.");
+        }
+
+        b = false;
+
+    }
+}
---
+++ Scope/src/Scope.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+class Scope {
+    public static void main(String[] args) {
+        int x;
+        x = 10;
+        if (x == 10) {
+            int y = 20;
+            System.out.println("X and Y: " + x + " " + y);
+            x = y * 2;
+        }
+        // y = 100; // Error: y is not known here
+
+        System.out.println("X is " + x);
+    }
+}
