bee072e8e6eefdc4ef1e49caad9a5f93bb2b1714
Rishabh Gupta <programmer.rishabh@gmail.com>
2024-08-23 16:11:15 +0530

    AutoArray

---
+++ AutoArray/src/AutoArray.java
@@ -0,0 +1,6 @@
+class AutoArray {
+    public static void main(String[] args) {
+        int[] monthArray = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("April has " + monthArray[3] + " days");
+    }
+}
