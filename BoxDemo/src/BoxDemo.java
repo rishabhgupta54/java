3501202cc3b846b64092e81c2cf9b83cd3def039
Rishabh Gupta <programmer.rishabh@gmail.com>
2024-08-28 00:00:34 +0530

    instance variables are not shared across the object

--- BoxDemo/src/BoxDemo.java
+++ BoxDemo/src/BoxDemo.java
@@ -5,8 +5,16 @@ class BoxDemo {
         box1.width = 20;
         box1.depth = 30;
 
-        double volume = box1.height * box1.width * box1.depth;
+        double volume1 = box1.height * box1.width * box1.depth;
 
-        System.out.println("Volume of the box is: " + volume);
+        Box box2 = new Box();
+        box2.height = 7.6;
+        box2.width = 8.7;
+        box2.depth = 9.8;
+
+        double volume2 = box2.height * box2.width * box2.depth;
+
+        System.out.println("Volume of the box1 is: " + volume1);
+        System.out.println("Volume of the box2 is: " + volume2);
     }
 }
