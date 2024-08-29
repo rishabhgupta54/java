5c6c4cebb9b532a74d3e29cef7e2778eb123cd65
Rishabh Gupta <programmer.rishabh@gmail.com>
2024-08-29 23:09:58 +0530

    Introduction to String

---
+++ StringDemo/src/StringDemo.java
@@ -0,0 +1,11 @@
+class StringDemo {
+    public static void main(String[] args) {
+        String strOne = "First String";
+        String strTwo = "Second String";
+        String strThree = strTwo + " and " + strTwo;
+
+        System.out.println(strOne);
+        System.out.println(strTwo);
+        System.out.println(strThree);
+    }
+}
