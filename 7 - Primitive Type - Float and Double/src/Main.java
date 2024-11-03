ced36fc771c0d6b0d06677c0e1056ce763f1d3da
Rishabh Gupta <programmer.rishabh@gmail.com>
2024-11-03 20:01:19 +0530

    Primitive Type - Fload and double

---
+++ 7 - Primitive Type - Float and Double/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        float floatMin = Float.MIN_VALUE;
+        float floatMax = Float.MAX_VALUE;
+        System.out.println("Float Min Value: " + floatMin);
+        System.out.println("Float Max Value: " + floatMax);
+
+        double doubleMin = Double.MIN_VALUE;
+        double doubleMax = Double.MAX_VALUE;
+
+        System.out.println("Double Min Value: " + doubleMin);
+        System.out.println("Double Max Value: " + doubleMax);
+
+        int intValue = 5 / 3;
+        float floatValue = 5f / 3f;
+        double doubleValue = 5.0 / 3.0;
+
+        System.out.println("int value: " + intValue);
+        System.out.println("float value: " + floatValue);
+        System.out.println("double value: " + doubleValue);
+    }
+}
