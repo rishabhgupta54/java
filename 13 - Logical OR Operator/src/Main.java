0fe862d45507dc5385da706d5699d742cd772a84
Rishabh Gupta <programmer.rishabh@gmail.com>
2024-11-05 20:25:06 +0530

    Logical Operators

---
+++ 13 - Logical OR Operator/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int topScore = 80;
+        int secondTopScore = 81;
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+    }
+}
---
+++ 14 - Assignment Operator VS Equals to Operator/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] rags) {
+        int newValue = 50;
+//        if (newValue = 50) {
+//            System.out.println("newValue = 50");
+//        }
+
+        if (newValue == 50) {
+            System.out.println("newValue == 50");
+        }
+
+        boolean isCar = false;
+        if(isCar = true) {
+            System.out.println("isCar = true");
+        }
+
+        if(isCar == true) {
+            System.out.println("isCar == true");
+        }
+
+        if(isCar != true) {
+            System.out.println("isCar != true");
+        }
+
+        if(!isCar) {
+            System.out.println("!isCar");
+        }
+    }
+}
