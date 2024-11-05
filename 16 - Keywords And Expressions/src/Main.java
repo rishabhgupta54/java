afdcbef4a7a8deb565dba6310f492ddb1c417a51
Rishabh Gupta <programmer.rishabh@gmail.com>
2024-11-06 01:04:32 +0530

    Expression

---
+++ 16 - Keywords And Expressions/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double kilometers = 100 * 1.609344; // kilometers = 100 * 1.609344
+        int highScore = 50; // highScore = 50
+        if (highScore == 50) { // (highScore == 50)
+            System.out.println("This is an expression"); // "This is an expression"
+        }
+    }
+}
